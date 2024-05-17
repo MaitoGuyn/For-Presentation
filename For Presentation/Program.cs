4988f1394bf48165712dd9f1ec982d9d27a1c284
user <user@DESKTOP-RU97V21>
2024-05-17 17:22:40 +0300

    Добавьте файлы проекта.

---
+++ For Presentation/Program.cs
@@ -0,0 +1,37 @@
+﻿bool IsPathCrossing(string path)
+{
+    List<Tuple<int, int>> positions = new List<Tuple<int, int>>();
+    int x = 0;
+    int y = 0;
+    positions.Add(new Tuple<int, int>(x, y));
+
+    foreach (char direction in path)
+    {
+        if (direction == 'N')
+        {
+            y += 1;
+        }
+        if (direction == 'S')
+        {
+            y -= 1;
+        }
+        if (direction == 'E')
+        {
+            x += 1;
+        }
+        if (direction == 'W')
+        {
+            x -= 1;
+        }
+
+        Tuple<int, int> newPosition = new Tuple<int, int>(x, y);
+        if (positions.Contains(newPosition))
+        {
+            return true;
+        }
+        positions.Add(newPosition);
+    }
+    return false;
+}
+Console.WriteLine(IsPathCrossing("NES"));
+
